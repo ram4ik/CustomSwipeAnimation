405e8237f41520654f3d4070adfd14503b7e5b7a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-21 09:51:30 +0300

    init

--- CustomSwipeAnimation/ContentView.swift
+++ CustomSwipeAnimation/ContentView.swift
@@ -9,8 +9,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var backViewSize: CGFloat = 80
+    @State var size: CGSize = .zero
+    
     var body: some View {
-        Text("Hello World")
+            
+        // geometry property is used to resad parent view size or coordinates...
+        GeometryReader { reader in
+            ZStack {
+                VStack {
+                    Color.blue
+                }.frame(width: reader.size.width - 160, height: 300)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(y: -30)
+                
+                VStack {
+                    Color.orange
+                }.frame(width: reader.size.width - self.backViewSize, height: 300)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                    .offset(y: -15)
+                
+                VStack {
+                    Color.red
+                }.frame(width: reader.size.width - 50, height: 300)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                    .gesture(DragGesture().onChanged({ (value) in
+                        self.size = value.translation
+                        self.backViewSize = 50
+                    }).onEnded({ (value) in
+                        self.size = .zero
+                        self.backViewSize = 80
+                    }))
+                    .offset(self.size)
+            }
+        }
+        .animation(.spring())
     }
 }
 
@@ -19,3 +56,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+   
+// you can use your custom animations and effects to your card...
